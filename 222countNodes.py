575e0d5a0d18b8d5b6374d084318c0fa4dafb840
李明博E0012960 <limingbo@focusmedia.cn>
2020-03-09 10:47:11 +0800

    maxProfit

--- 222countNodes.py
+++ 222countNodes.py
@@ -1,3 +1,25 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-class solution():
-    pass
+
+class Solution(object):
+    def countNodes(self, root):
+        if root is None:
+            return 0
+        depth_left = 0
+        depth_right = 0
+        root1 = root
+        root2 = root
+        while root1.left is not None:
+            root1 = root1.left
+            depth_left += 1
+        while root2.right is not None:
+            root2 = root2.right
+            depth_right += 1
+        if depth_right == depth_left:
+            return 1 + 2 * (2 ** depth_left - 1)
+        else:
+            return 1 + self.countNodes(root.left) + 1 + self.countNodes(root.right)
