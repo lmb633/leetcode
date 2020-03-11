612a1836f1154963d6d20388aa541590d77b6a30
李明博E0012960 <limingbo@focusmedia.cn>
2020-03-11 10:47:38 +0800

    maxSubArray

--- 42maxSubArray.py
+++ 42maxSubArray.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        temp_max=0
-        max_=-2**32
+        temp_max = 0
+        max_ = -2 ** 32
         for i in range(len(nums)):
-            temp_max+=nums[i]
-            if temp_max>max_:
-                max_=temp_max
-            if temp_max<0:
-                temp_max=0
+            temp_max += nums[i]
+            if temp_max > max_:
+                max_ = temp_max
+            if temp_max < 0:
+                temp_max = 0
         return max_
