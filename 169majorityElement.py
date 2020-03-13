3db648afeeca163960a2565eef70cf9036e476b8
李明博E0012960 <limingbo@focusmedia.cn>
2020-03-13 12:09:00 +0800

    majorityElement

--- 169majorityElement.py
+++ 169majorityElement.py
@@ -9,3 +9,18 @@ class Solution:
                 hash_set[num] = hash_set[num] + 1
             if hash_set[num] > mid:
                 return num
+
+
+class Solution2:
+    def majorityElement(self, nums):
+        candidate = nums[0]
+        count = 1
+        for num in nums[1:]:
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+            if count == -1:
+                candidate = num
+                count = 0
+        return candidate
