12b38e98bc99423c2858d7551fe1bca51b3d1444
李明博E0012960 <limingbo@focusmedia.cn>
2020-03-25 11:10:32 +0800

    reverseList

--- 206reverseList.py
+++ 206reverseList.py
@@ -3,17 +3,29 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+
 class Solution(object):
     def reverseList(self, head):
-        if head is None or head.next is None :
+        if head is None or head.next is None:
             return head
-        right=head.next
-        left=None
+        right = head.next
+        left = None
         while True:
-            head.next=left
-            left=head
-            head=right
-            right=right.next
+            head.next = left
+            left = head
+            head = right
+            right = right.next
             if right is None:
-                head.next=left
+                head.next = left
                 return head
+
+
+class Solution2(object):
+    def reverseList(self, head):
+        head0 = None
+        while head is not None:
+            temp = head
+            head = head.next
+            temp.next = head0
+            head0 = temp
+        return head0
