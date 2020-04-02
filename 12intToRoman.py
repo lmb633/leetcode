d0078d5fe7bd0b0b8d573bbff3220f97b6832fb2
李明博E0012960 <limingbo@focusmedia.cn>
2020-04-02 09:18:20 +0800

    format

--- 12intToRoman.py
+++ 12intToRoman.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def intToRoman(self, num):
-        trans_map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
-        trans_map={v:k for k,v in trans_map.items()}
+        trans_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        trans_map = {v: k for k, v in trans_map.items()}
         print(trans_map)
-        numbers=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-        result=''
-        while num>=1:
+        numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ''
+        while num >= 1:
             for n in numbers:
-                if num>=n:
-                    result+=trans_map[n]
-                    num-=n
+                if num >= n:
+                    result += trans_map[n]
+                    num -= n
                     break
         return result
         """
--- 62lastRemaining.py
+++ 62lastRemaining.py
@@ -1,6 +1,7 @@
+# 约瑟夫环
 class Solution(object):
     def lastRemaining(self, n, m):
-        result=0
-        for i in range(1,n+1):
-            result=(result+m)%i
+        result = 0
+        for i in range(1, n + 1):
+            result = (result + m) % i
         return result
