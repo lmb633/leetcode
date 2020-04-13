8d86550fb088997f74f1eccf6fc1ecdf61a4b278
李明博E0012960 <limingbo@focusmedia.cn>
2020-04-13 15:24:39 +0800

    longestPalindromeSubseq

---
+++ 647countSubstrings.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        length = len(s)
+        if length < 1:
+            return 0
+        dp = [[0] * length for _ in range(length)]
+        for i in range(length):
+            dp[i][i] = 1
+        for k in range(1, length):
+            for i in range(length - k):
+                if s[i] == s[i + k]:
+                    if k == 1:
+                        dp[i][i + k] = 1
+                    else:
+                        dp[i][i + k] = dp[i + 1][i + k - 1]
+                else:
+                    dp[i][i + k] = 0
+        # print(dp)
+        result = 0
+        for l in dp:
+            result += sum(l)
+        return result
+
+        """
+        :type s: str
+        :rtype: int
+        """
