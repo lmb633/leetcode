68971bdc65687c15836d22efb63131c8b8115714
李明博E0012960 <limingbo@focusmedia.cn>
2020-04-28 15:29:06 +0800

    maxProfit4

---
+++ 188maxProfit.py
@@ -0,0 +1,30 @@
+# k=any
+class Solution(object):
+    def maxProfit(self, prices, k):
+        length = len(prices)
+        if k > length // 2:
+            return self.maxProfit_any(prices)
+        dp = [[0, 0] for _ in range(k + 1)]
+        for i in range(k + 1):
+            dp[i][0] = 0
+            dp[i][1] = float('-inf')
+        for i in range(length):
+            for j in range(k, 0, -1):
+                dp[j][0] = max(dp[j][0], dp[j][1] + prices[i])
+                dp[j][1] = max(dp[j][1], dp[j - 1][0] - prices[i])
+        print(dp)
+        return max([dp[i][0] for i in range(k + 1)])
+
+    def maxProfit_any(self, prices):
+        profit = 0
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] > buy:
+                profit += prices[i] - buy
+            buy = prices[i]
+        return profit
+
+
+solution = Solution()
+a = [3, 2, 6, 5, 0, 3]
+print(solution.maxProfit(a, 2))
